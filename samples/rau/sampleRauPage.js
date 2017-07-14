278ff604d19ef44480edc0172a502a34d9a25a63
birfincankafein <metehantoksoy@gmail.com>
2017-07-14 15:13:33 +0000

    Added samples make version 1.1.2

---
+++ samples/rau/sampleRauPage.js
@@ -0,0 +1,46 @@
+const Page				= require('sf-core/ui/page');
+const extend			= require("js-base/core/extend");
+const Color 			= require('sf-core/ui/color');
+const Button			= require('sf-core/ui/button');
+const FlexLayout		= require("sf-core/ui/flexlayout");
+const RauLib			= require("sf-extension-utils/rau");
+
+var pageRAU = extend(Page)(
+	function(_super) {
+		_super(this);
+		
+		var btnRau = new Button({
+			height: 75,
+			width: 100,
+			margin: 10,
+			alignSelf: FlexLayout.AlignSelf.CENTER,
+			text: "Test RAU",
+			onPress: function(){
+				RauLib.checkUpdate();
+			}
+		});
+		
+		var btnRauWithOptions = new Button({
+			height: 75,
+			width: 100,
+			margin: 10,
+			alignSelf: FlexLayout.AlignSelf.CENTER,
+			text: "Test RAU Options",
+			onPress: function(){
+				RauLib.checkUpdate({
+					showProgressCheck: true,
+					showProgressErrorAlert: true
+				});
+			}
+		});
+		
+		this.layout.flexWrap = FlexLayout.FlexWrap.WRAP;
+		this.layout.flexDirection = FlexLayout.FlexDirection.ROW;
+		this.layout.justifyContent = FlexLayout.JustifyContent.CENTER;
+
+		this.layout.addChild(btnRau);
+		this.layout.addChild(btnRauWithOptions);
+	}
+);
+
+module.exports = pageRAU;
