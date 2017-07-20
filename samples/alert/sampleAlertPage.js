d7cf4cb5a3bf8279c1e7c9cd659e797101ca9ae2
birfincankafein <metehantoksoy@gmail.com>
2017-07-20 12:43:03 +0000

    Added alertutil and sample

---
+++ samples/alert/sampleAlertPage.js
@@ -0,0 +1,61 @@
+const Page          = require("sf-core/ui/page");
+const extend        = require("js-base/core/extend");
+const Button        = require('sf-core/ui/button');
+const FlexLayout    = require('sf-core/ui/flexlayout');
+const AlertViewUtil = require('sf-extension-utils/alert');
+
+var pageAlert = extend(Page)(
+    function(_super) {
+        _super(this);
+        
+        var myButtonAlertMessage = new Button({
+            text: 'Alert Message',
+            height: 75,
+            width: 200,
+            margin: 15,
+            alignSelf: FlexLayout.AlignSelf.CENTER,
+            onPress: function() {
+				AlertViewUtil.showAlert("Message");
+            }.bind(this)
+        });
+        
+        var myButtonAlertTitleMessage = new Button({
+            text: 'Alert Message with Title',
+            height: 75,
+            width: 200,
+            margin: 15,
+            alignSelf: FlexLayout.AlignSelf.CENTER,
+            onPress: function() {
+				AlertViewUtil.showAlert("Title", "Message");
+            }.bind(this)
+        });
+        
+        var myButtonAlertTitleMessageCallback = new Button({
+            text: 'Alert Message and Title with Callbacks',
+            height: 75,
+            width: 200,
+            margin: 15,
+            alignSelf: FlexLayout.AlignSelf.CENTER,
+            onPress: function() {
+				AlertViewUtil.showAlert("Title", "Message", {
+                    text: "Negative Button",
+                    callback: function(){
+                        AlertViewUtil.showAlert("NegativeButton Clicked!");
+                    }
+                }, {
+                    text: "Positive Button",
+                    callback: function(){
+                        AlertViewUtil.showAlert("Positive Button Clicked!");
+                    }
+                });
+            }.bind(this)
+        });
+        
+		this.layout.justifyContent = FlexLayout.JustifyContent.CENTER;
+        this.layout.addChild(myButtonAlertMessage);
+        this.layout.addChild(myButtonAlertTitleMessage);
+        this.layout.addChild(myButtonAlertTitleMessageCallback);
+    }
+);
+
+module.exports = pageAlert;
