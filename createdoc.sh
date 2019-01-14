4b52218698b3949e64e1f66e72b21ba54b064a54
Alper Özışık <alper.ozisik@smartface.io>
2019-01-14 13:09:15 +0000

    new utils for router

--- createdoc.sh
+++ createdoc.sh
@@ -14,4 +14,14 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
         echo "generting ./doc/${filename%%.*}.md"
         npx jsdoc2md -f "./$filename" > "../doc/${filename%%.*}.md"
     done
+    
+    cd router
+    for filename in *.js; do
+        if [[ "$filename" == "index.js" ]]; then
+            continue
+        fi
+
+        echo "generting ./doc/${filename%%.*}.md"
+        npx jsdoc2md -f "./$filename" > "../../doc/router/${filename%%.*}.md"
+    done
 )
---
+++ doc/router/goBack.md
@@ -0,0 +1,11 @@
+<a name="module_router"></a>
+
+## router : <code>object</code>
+Adds default goBack action on pages
+Sets android onBackButtonPressed
+If a pageInstance has shouldExit, exits the application
+Handles router.goBack and router.dismiss automatically
+Custome behaviour can be overwritten by setting pageInstance.goBack
+
+**Author**: Alper Ozisik <alper.ozisik@smartface.io>  
+**Copyright**: Smartface 2019  
