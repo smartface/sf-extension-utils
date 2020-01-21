0bd779baa0188609338aead232f02dfc80e89a08
ozcanovunc <ozcanovunc@outlook.com>
2020-01-21 15:59:05 +0300

    fix: Require of CircularProgressBar

--- createdoc.sh
+++ createdoc.sh
@@ -42,4 +42,14 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
         echo "generating ./doc/${filename%%.*}.md"
         npx jsdoc2md -f "./$filename" > "../../doc/components/${filename%%.*}.md"
     done
+
+    cd ../art
+    for filename in *.js; do
+        if [[ "$filename" == "index.js" ]]; then
+            continue
+        fi
+
+        echo "generating ./doc/${filename%%.*}.md"
+        npx jsdoc2md -f "./$filename" > "../../doc/art/${filename%%.*}.md"
+    done
 )
