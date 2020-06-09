16da5ef28abde245abb638cbc74129cff6472989
Furkan Arabacı <furkanarabaci@hotmail.com>
2020-06-09 11:19:47 +0300

    feature(typescript): add goBack

---
+++ lib/router/goBack/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Adds default goBack action on pages
+ * Sets android onBackButtonPressed
+ * If a pageInstance has shouldExit, exits the application
+ * Handles router.goBack and router.dismiss automatically
+ * Custome behaviour can be overwritten by setting pageInstance.goBack
+ * Has no exports.
+ * @module router
+ * @type {object}
+ * @author Alper Ozisik <alper.ozisik@smartface.io>
+ * @copyright Smartface 2019
+ */
