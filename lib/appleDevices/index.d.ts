0474b8213fab8a06360f4d71de4d4d372bcc401a
Furkan Arabaci <furkan.arabaci@smartface.io>
2021-07-07 12:32:15 +0300

    fix(appleDevices): Remove unnecessary require from d.ts

--- lib/appleDevices/index.d.ts
+++ lib/appleDevices/index.d.ts
@@ -6,10 +6,6 @@
  * @copyright Smartface 2020
  */
 
-const Hardware = require('sf-core/device/hardware');
-const System = require('sf-core/device/system');
-const deviceMapping = require("./deviceMapping.json");
-
 /**
  * Gets the human readable modelname for iphone devies.
  * Returns empty string on Android devices.
