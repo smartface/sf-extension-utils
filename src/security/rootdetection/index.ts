e3fdedf3fcd7d77769faf1ff0bd1dd58e0e3c2ca
Furkan Arabacı <furkan.arabaci@smartface.io>
2021-08-22 10:57:24 +0300

    feature(typescript): Tidy up rootdetection

--- src/security/rootdetection/index.ts
+++ src/security/rootdetection/index.ts
@@ -9,6 +9,21 @@
  */
 
 import System from "@smartface/native/device/system";
+import AndroidRootConfig from 'security/rootdetection/androidRoot.json';
+import iOSJailBreakConfig from 'security/rootdetection/iOSJailBreak.json';
+
+let rootDetectionNative: any;
+
+if (System.OS === System.OSType.IOS) {
+	//@ts-ignore
+	rootDetectionNative = new __SF_RootDetection();
+} else {
+	//@ts-ignore
+	const RootDetectionUtil = requireClass(
+		"io.smartface.android.utils.RootDetectionUtil"
+	);
+	rootDetectionNative = new RootDetectionUtil();
+}
 
 /**
  * Includes a few established methods to capture whether device is
@@ -20,95 +35,31 @@ import System from "@smartface/native/device/system";
  * @class
  *
  */
-class RootDetection {
+export default abstract class RootDetection {
 	rootDetection: any;
-	constructor() {
-		if (System.OS === System.OSType.IOS) {
-			//@ts-ignore
-			this.rootDetection = new __SF_RootDetection();
-		} else {
-			//@ts-ignore
-			const RootDetectionUtil = requireClass(
-				"io.smartface.android.utils.RootDetectionUtil"
-			);
-			this.rootDetection = new RootDetectionUtil();
-		}
-	}
 
-	private checkPathsForIOS(): boolean {
-		this.rootDetection.pathsToCheck = [
-			"/Applications/Cydia.app",
-			"/Library/MobileSubstrate/MobileSubstrate.dylib",
-			"/bin/bash",
-			"/usr/sbin/sshd",
-			"/etc/apt",
-			"/private/var/lib/apt",
-			"/usr/sbin/frida-server",
-			"/usr/bin/cycript",
-			"/usr/local/bin/cycript",
-			"/usr/lib/libcycript.dylib",
-			"/Applications/FakeCarrier.app",
-			"/Applications/Icy.app",
-			"/Applications/IntelliScreen.app",
-			"/Applications/MxTube.app",
-			"/Applications/RockApp.app",
-			"/Applications/SBSettings.app",
-			"/Applications/WinterBoard.app",
-			"/Applications/blackra1n.app",
-			"/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist",
-			"/Library/MobileSubstrate/DynamicLibraries/Veency.plist",
-			"/System/Library/LaunchDaemons/com.ikey.bbot.plist",
-			"/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist",
-			"/bin/sh",
-			"/etc/ssh/sshd_config",
-			"/private/var/lib/cydia",
-			"/private/var/mobile/Library/SBSettings/Themes",
-			"/private/var/stash",
-			"/private/var/tmp/cydia.log",
-			"/usr/bin/sshd",
-			"/usr/libexec/sftp-server",
-			"/usr/libexec/ssh-keysign",
-			"/var/cache/apt",
-			"/var/lib/apt",
-			"/var/lib/cydia",
-		];
-		return this.rootDetection.checkPaths();
+	private static checkPathsForIOS(): boolean {
+		rootDetectionNative.pathsToCheck = iOSJailBreakConfig.pathsToCheck;
+		return rootDetectionNative.checkPaths();
 	}
-	private checkPathsForAndroid(): boolean {
-		const knownRootAppPackages = [
-			"com.noshufou.android.su",
-			"com.noshufou.android.su.elite",
-			"eu.chainfire.supersu",
-			"com.koushikdutta.superuser",
-			"com.thirdparty.superuser",
-			"com.yellowes.su",
-			"com.topjohnwu.magisk",
-			"kingoroot.supersu",
-		];
-		const isDetected = this.rootDetection.checkAppPackages(
+	private static checkPathsForAndroid(): boolean {
+		const knownRootAppPackages = AndroidRootConfig.knownRootAppPackages;
+		const isDetected = rootDetectionNative.checkAppPackages(
 			//@ts-ignore
 			array(knownRootAppPackages, "java.lang.String")
 		);
 		return isDetected;
 	}
-	private checkSchemes(): boolean {
-		this.rootDetection.schemesToCheck = ["cydia://package/com.example.package"];
-		return this.rootDetection.checkSchemes();
+	private static checkSchemes(): boolean {
+		rootDetectionNative.schemesToCheck = iOSJailBreakConfig.schemesToCheck;
+		return rootDetectionNative.checkSchemes();
 	}
-	private checkRootAccessGainedForIOS(): boolean {
-		return this.rootDetection.canViolateSandbox();
+	private static checkRootAccessGainedForIOS(): boolean {
+		return rootDetectionNative.canViolateSandbox();
 	}
-	private checkRootAccessGainedForAndroid(): boolean {
-		const pathThatShouldNotWritable = [
-			"/system",
-			"/system/bin",
-			"/system/sbin",
-			"/system/xbin",
-			"/vendor/bin",
-			"/sbin",
-			"/etc",
-		];
-		const isWritable = this.rootDetection.checkRootAccessGained(
+	private static checkRootAccessGainedForAndroid(): boolean {
+		const pathThatShouldNotWritable =  AndroidRootConfig.knownRootAppPackages;
+		const isWritable = rootDetectionNative.checkRootAccessGained(
 			//@ts-ignore
 			array(pathThatShouldNotWritable, "java.lang.String")
 		);
@@ -120,7 +71,7 @@ class RootDetection {
 	 * @method
 	 * @return {boolean} - returns true in case of device rooted. Otherwise returns false.
 	 * @public
-	 * @instance
+	 * @static
 	 * @example
 	 *```
 	 * import RootDetection from '@smartface/extension-utils/lib/security/rootdetection;
@@ -132,40 +83,20 @@ class RootDetection {
 	 * }
 	 *```
 	 */
-	isRooted(): boolean {
+	static isRooted(): boolean {
 		if (System.OS === System.OSType.IOS){
-			return this.checkPathsForIOS() || this.checkSchemes() || this.checkRootAccessGainedForIOS();
+			return RootDetection.checkPathsForIOS() || RootDetection.checkSchemes() || RootDetection.checkRootAccessGainedForIOS();
 		}
 		else {
-			return this.checkPathsForAndroid() || this.checkSuBinaryExistance() || this.checkRootAccessGainedForAndroid();
+			return RootDetection.checkPathsForAndroid() || RootDetection.checkSuBinaryExistance() || RootDetection.checkRootAccessGainedForAndroid();
 		}
 	}
 
-	private checkSuBinaryExistance(): boolean {
-		const suBinaryPaths = [
-			"/data/local/",
-			"/data/local/bin/",
-			"/data/local/xbin/",
-			"/sbin/",
-			"/su/bin/",
-			"/system/bin/",
-			"/system/bin/.ext/",
-			"/system/bin/failsafe/",
-			"/system/sd/xbin/",
-			"/system/usr/we-need-root/",
-			"/system/xbin/",
-			"/cache/",
-			"/data/",
-			"/dev/"
-		];
-		const isThereSuBinaries = this.rootDetection.checkSuBinaryExistance(
+	private static checkSuBinaryExistance(): boolean {
+		const suBinaryPaths = AndroidRootConfig.suBinaryPaths;
+		const isThereSuBinaries = rootDetectionNative.checkSuBinaryExistance(
 			//@ts-ignore
 			array(suBinaryPaths, "java.lang.String"));
 		return isThereSuBinaries;
 	}
 }
-
-const rootDetection = new RootDetection();
-Object.freeze(rootDetection);
-
-export default rootDetection;
